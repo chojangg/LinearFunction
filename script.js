ab937fe38b5ce6a3f0145a53b47544c4419324cb
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-11-11 09:52:59 +0900

    학습모델 구조(인공신경망 2개)

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ async function getData() {
   return cleaned;
 }
 
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential();
+
+  // Add a single input layer
+  model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+
+  // Add an output layer
+  model.add(tf.layers.dense({units: 1, useBias: true}));
+
+  return model;
+}
+
 async function run() {
   // Load and plot the original input data that we are going to train on.
   const data = await getData();
@@ -31,6 +44,10 @@ async function run() {
       height: 300
     }
   );
+
+  // Create the model
+  const model = createModel();
+  tfvis.show.modelSummary({name: 'Model Summary'}, model);
 
   // More code will be added below
 }
